c504ec8fa99b20608072fdb3e42a5542f22ed7c0
TylerH800 <552553@chrome.escg.ac.uk>
2024-09-30 11:05:29 +0100

    Camera Smoothing
    Made the camera follow the player smoother

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,10 @@ public class CameraFollow : MonoBehaviour
 {
     private GameObject player;
     private Vector3 offset = new Vector3(0, 2, -10);
+    public float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] private Transform target;
     
 
     void Start()
@@ -13,8 +17,13 @@ public class CameraFollow : MonoBehaviour
         player = GameObject.Find("Player");
     }
 
-    void LateUpdate()
-    {//camera follows player position
-        transform.position = player.transform.position + offset;
+    void Update()
+    {
+        //camera follows player position
+        Vector3 targetPos = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+
+
+        
     }
 }
